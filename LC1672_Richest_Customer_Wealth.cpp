7bd28195e8abd7c3a34158d70816050f7034f0ab
prasannabantu <25prasanna25@gmail.com>
2022-03-07 01:27:33 +0530

    1672. Richest Customer Wealth

--- LC1672_Richest_Customer_Wealth.cpp
+++ LC1672_Richest_Customer_Wealth.cpp
@@ -1,6 +1,19 @@
 //1672. Richest Customer Wealth
+//
+// You are given an m x n integer grid accounts
+// where accounts[i][j] is the amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank.
+// Return the wealth that the richest customer has.
+// A customer's wealth is the amount of money they have in all their bank accounts. The richest customer is the customer that has the maximum wealth.
 
-You are given an m x n integer grid accounts
-where accounts[i][j] is the amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank.
-Return the wealth that the richest customer has.
-A customer's wealth is the amount of money they have in all their bank accounts. The richest customer is the customer that has the maximum wealth.
+class Solution {
+public:
+     int maximumWealth(vector<vector<int>>& accounts) {
+           int ans=0,sum=0;
+          for(int i=0;i<accounts.size();i++)
+           {
+                sum=accumulate(accounts[i].begin(),accounts[i].end(),0);
+             ans=max(ans,sum);
+           }
+          return  ans;
+  }
+};
