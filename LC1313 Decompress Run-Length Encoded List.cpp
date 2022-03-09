917d2fa1d80ad19478b318a6243498e97456f3df
prasannabantu <25prasanna25@gmail.com>
2022-03-09 18:30:33 +0530

    LC1313. Decompress Run-Length Encoded List

---
+++ LC1313 Decompress Run-Length Encoded List.cpp
@@ -0,0 +1,16 @@
+// 1313. Decompress Run-Length Encoded List
+
+
+class Solution {
+public:
+    vector<int> decompressRLElist(vector<int>& nums) {
+        vector<int> ans;
+        int n=nums.size();
+        for(int i=0;i<n;i+=2)
+        {
+            for(int j=0;j<nums[i];j++)
+                ans.push_back(nums[i+1]);
+        }
+        return ans;
+    }
+};
