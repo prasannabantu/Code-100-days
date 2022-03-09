07010d3c355e31336e4f6b12259acf6ed72695da
prasannabantu <25prasanna25@gmail.com>
2022-03-09 18:25:13 +0530

    LC1512 100% faster

--- LC1512 Number of Good Pairs.cpp
+++ LC1512 Number of Good Pairs.cpp
@@ -15,3 +15,23 @@ public:
         return count;
     }
 };
+
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& a) {
+        int n=a.size();
+        vector<int>ans(101,0);
+        for(int i=0;i<n;i++)
+            ans[a[i]]++;
+        int count=0;
+        for(int i=0;i<=100;i++)
+            if(ans[i]>0)
+            count+=nn(ans[i]-1);
+        return count;
+    }
+
+    int nn(int n)
+    {
+        return (n*(n+1))/2;
+    }
+};
