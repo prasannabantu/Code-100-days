ab290242a4983e3d75e19f44b5f197996c9541b2
prasannabantu <25prasanna25@gmail.com>
2022-03-12 23:15:10 +0530

    same

--- LC1304 Find N Unique Integers Sum up to Zero.cpp
+++ LC1304 Find N Unique Integers Sum up to Zero.cpp
@@ -1,4 +1,4 @@
-
+// LC1304 Find N Unique Integers Sum up to Zero.
 
 class Solution {
 public:
@@ -22,6 +22,24 @@ public:
             }
 
         }
+        return a;
+    }
+};
+
+class Solution {
+public:
+    vector<int> sumZero(int n) {
+       vector<int>a;
+            if(n%2!=0)
+            a.push_back(0);
+            n/=2;
+
+            for(int i=1;i<=n;i++)
+            {
+                a.push_back(i+1);
+                a.push_back(-1*(i+1));
+            }
+
         return a;
     }
 };
