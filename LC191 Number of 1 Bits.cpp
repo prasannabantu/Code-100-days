c013bc492f390f46289f8cd722748ab5496e18ca
prasannabantu <25prasanna25@gmail.com>
2022-03-18 12:44:30 +0530

    LC191 Number of 1 Bits

---
+++ LC191 Number of 1 Bits.cpp
@@ -0,0 +1,13 @@
+191. Number of 1 Bits
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int c=0;
+        while(n>0)
+        {
+            c+=n&1;
+            n=n>>1;
+        }
+        return c;
+    }
+};
