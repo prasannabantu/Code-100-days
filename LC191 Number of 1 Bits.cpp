9da4b998a6c0a066f5cec1ed15eec8d742df5da1
prasannabantu <25prasanna25@gmail.com>
2022-03-18 13:04:39 +0530

    LC191 Number of 1 Bits 4 approaches

--- LC191 Number of 1 Bits.cpp
+++ LC191 Number of 1 Bits.cpp
@@ -1,4 +1,18 @@
-191. Number of 1 Bits
+// LC191 Number of 1 Bits
+
+
+
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        bitset<32>b(n);
+        return b.count();
+    }
+};
+
+
+
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
@@ -9,5 +23,27 @@ public:
             n=n>>1;
         }
         return c;
+    }
+};
+
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+       int c=0;
+        while(n)
+        {
+            n=n&(n-1);
+            c++;
+        }
+        return c;
+    }
+};
+
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+       return __builtin_popcount(n);
     }
 };
