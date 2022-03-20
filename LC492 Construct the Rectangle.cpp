421763de55e3edbb0dd20b0b5518946258722810
prasannabantu <25prasanna25@gmail.com>
2022-03-20 11:53:34 +0530

    Made it a little smaller

--- LC492 Construct the Rectangle.cpp
+++ LC492 Construct the Rectangle.cpp
@@ -1,1 +1,19 @@
-LC492 Construct the Rectangle
+class Solution {
+public:
+    vector<int> constructRectangle(int area) {
+        vector<int>ans;
+        int sq=sqrt(area);
+        for(int i=sq;i>=1;i--)
+        {
+            if(area%i==0)
+            {
+                   ans.push_back(area/i);
+                   ans.push_back(i);
+                   break;
+            }
+        }
+
+        return ans;
+    }
+
+ };
