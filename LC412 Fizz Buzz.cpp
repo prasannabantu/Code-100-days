38dc939c55c2f27a7e099282649df51eda64e82f
prasannabantu <25prasanna25@gmail.com>
2022-03-20 12:14:12 +0530

    LC412 Fizz Buzz (little faster)

--- LC412 Fizz Buzz.cpp
+++ LC412 Fizz Buzz.cpp
@@ -14,3 +14,34 @@ public:
         return  ans;
     }
 };
+
+
+
+
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+           vector<string>ans;
+        for(int i=0;i<n;i++)
+        {
+            ans.push_back(to_string(i+1));
+        }
+
+        for(int i=2;i<n;i+=3)
+        {
+            ans[i]="Fizz";
+        }
+
+        for(int i=4;i<n;i+=5)
+        {
+            ans[i]="Buzz";
+        }
+        
+
+        for(int i=14;i<n;i+=15)
+        {
+            ans[i]="FizzBuzz";
+        }
+        return  ans;
+    }
+};
