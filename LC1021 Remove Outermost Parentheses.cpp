f1f0ee106c443b4306807298d525d222801cdd5e
prasannabantu <25prasanna25@gmail.com>
2022-03-21 11:01:31 +0530

    LC1021 Remove Outermost Parentheses

---
+++ LC1021 Remove Outermost Parentheses.cpp
@@ -0,0 +1,30 @@
+//LC1021 Remove Outermost Parentheses
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        stack<char> st;
+        string ans;
+
+        for(auto i:s)
+        {
+            if(!st.empty())
+            if(st.size()!=0 && i=='(')
+            {
+                ans.push_back(i);
+            }
+
+            if(i=='(') st.push(i);
+            else
+            {
+                if(!st.empty())
+                {
+                if(st.size()!=1) ans.push_back(i);
+                st.pop();
+                }
+            }
+
+
+        }
+        return ans;
+    }
+};
