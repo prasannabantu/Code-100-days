1045221f1c26b6a7cc0907d4014b33cf1e5d58ee
prasannabantu <25prasanna25@gmail.com>
2022-03-22 15:11:12 +0530

    LC1844 Replace All Digits with Characters

---
+++ LC1844 Replace All Digits with Characters.cpp
@@ -0,0 +1,12 @@
+LC1844 Replace All Digits with Characters
+class Solution {
+public:
+    string replaceDigits(string s) {
+        int n=s.size();
+        for(int i=1;i<n;i+=2)
+        {
+            s[i]=s[i-1]+(s[i]-'0');
+        }
+        return s;
+    }
+};
