9f20d39868cc0b6fe46bd70e6c220c613416adf5
prasannabantu <25prasanna25@gmail.com>
2022-03-22 23:48:30 +0530

    0ms

--- LC1678 Goal Parser Interpretation.cpp
+++ LC1678 Goal Parser Interpretation.cpp
@@ -1,5 +1,29 @@
 //LC1678 Goal Parser Interpretation
 
+class Solution {
+public:
+    string interpret(string c) {
+        string ans="";
+        int n=c.size();
+        for(int i=0;i<n;i++)
+        {
+            if(c[i]=='G') ans.push_back('G');
+            if(c[i]=='(')
+            {
+                if(c[i+1]==')')
+                {
+                    ans.push_back('o'); i++;
+                }
+                else
+                {
+                    ans+="al";
+                    i+=3;
+                }
+            }
+        }
+        return ans;
+    }
+};
 
 class Solution {
 public:
