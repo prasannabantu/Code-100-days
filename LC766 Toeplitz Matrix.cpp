88a55176ce0bb3cf8fc9162e51cb524fc0972782
prasannabantu <25prasanna25@gmail.com>
2022-03-23 17:25:53 +0530

    LC766 Toeplitz Matrix

---
+++ LC766 Toeplitz Matrix.cpp
@@ -0,0 +1,17 @@
+LC766 Toeplitz Matrix
+class Solution {
+public:
+    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+       int m=matrix.size();
+       int n=matrix[0].size(); int ans=0;
+
+       for(int i=0;i<m-1;i++)
+       {
+           for(int j=0;j<n-1;j++)
+           {
+               if(matrix[i][j]!=matrix[i+1][j+1])  return 0;
+           }
+       }
+        return 1;
+    }
+};
