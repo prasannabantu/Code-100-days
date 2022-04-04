14cac265e4098214b6d03dab43cff1e75b7d6523
prasannabantu <25prasanna25@gmail.com>
2022-04-04 14:33:13 +0530

    LC56. Merge Intervals

---
+++ LC56. Merge Intervals.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    static bool cmp(vector<int>&a,vector<int>&b)
+    {
+        if(a[0]<b[0]) return 1;
+        return 0;
+    }
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int n=intervals.size();
+       sort(intervals.begin(),intervals.end());
+        vector<vector<int>>ans;
+        ans.push_back(intervals[0]);
+        for(int i=1;i<n;i++)
+        {
+            if(ans.back()[1]>=intervals[i][0]) ans.back()[1]=max(ans.back()[1],intervals[i][1]);
+            else ans.push_back(intervals[i]);
+        }
+        return ans;
+
+    }
+
+};
