c84ad766a88a22bbcefbc6c8365dc30f514d9541
prasannabantu <25prasanna25@gmail.com>
2022-04-05 22:13:35 +0530

    LC167. Two Sum II - Input Array Is Sorted.cpp

---
+++ LC167. Two Sum II - Input Array Is Sorted.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& a, int target) {
+        int n=a.size();
+        int l=0; int r=n-1;
+        for(int i=0;i<n-1;i++)
+        {
+            int ele=target-a[i];
+            l=i+1; r=n-1;
+            while(l<=r)
+            {
+                int m=l+(r-l)/2;
+                if(a[m]==ele) return {i+1,m+1};
+                if(a[m]>ele) r=m-1;
+                else l=m+1;
+            }
+        }
+        return {-1,-1};
+    }
+};
