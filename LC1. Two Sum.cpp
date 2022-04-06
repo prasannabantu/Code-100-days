cda419dbf646ab3c2342670c00232f1ceed5d314
prasannabantu <25prasanna25@gmail.com>
2022-04-06 14:19:32 +0530

    // 1. Two Sum

--- LC1. Two Sum.cpp
+++ LC1. Two Sum.cpp
@@ -1,1 +1,13 @@
-1. Two Sum
+// 1. Two Sum
+vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int>um;
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            if(!(um.find(target-nums[i])==um.end()))
+                return {um[target-nums[i]],i};
+            else
+                um[nums[i]]=i;
+        }
+        return {};
+    }
