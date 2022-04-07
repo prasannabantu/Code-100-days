4d40646a2f3c21e96ee608d9a083449807e0d6d0
prasannabantu <25prasanna25@gmail.com>
2022-04-07 21:58:15 +0530

    LC1046

---
+++ LC1046. Last Stone Weight.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+          priority_queue<int> p(stones.begin(),stones.end());
+          while(p.size()>1)
+          {
+              int a=p.top();
+              p.pop();
+              int b=p.top();
+              p.pop();
+              p.push(a-b);
+          }
+        return p.top();
+    }
+
+};
