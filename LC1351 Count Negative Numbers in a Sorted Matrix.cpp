091c4fedea528fed29dadbbc209aa2d602bba30d
prasannabantu <25prasanna25@gmail.com>
2022-04-08 09:21:52 +0530

    LC1351 Count Negative Numbers in a Sorted Matrix Learnt that j=n is ok too

--- LC1351 Count Negative Numbers in a Sorted Matrix.cpp
+++ LC1351 Count Negative Numbers in a Sorted Matrix.cpp
@@ -16,3 +16,20 @@ public:
         return c;
     }
 };
+
+class Solution {
+public:
+    int countNegatives(vector<vector<int>>& g) {
+        int c=0;
+        int m=g.size();
+        int n=g[0].size();
+        int i=0; int j=n;
+        while(i<m)
+        {
+            while(j-1>=0 && g[i][j-1]<0)  --j;
+            c+=n-j;
+            i++;
+        }
+        return c;
+    }
+};
