73221613be8c55763fe85700c39f74c5918b55f3
prasannabantu <25prasanna25@gmail.com>
2022-04-10 21:07:56 +0530

    small comment

--- LC58. Length of Last Word.cpp
+++ LC58. Length of Last Word.cpp
@@ -4,9 +4,11 @@ public:
         int i=0; int poi=0;
         int n=s.size();
         int j=n-1;
-        while(j>0 && s[j]==' ') j--; cout<<j<<" ";
+		//Just notice that i>=0 codition allows i to become -1 which will be useful with test cases like "Hello"
+		//because in the answer j-i becomes j+1 for such examples which would be the actual length of the string
+        while(j>0 && s[j]==' ') j--; //cout<<j<<" ";
         i=j;
-        while(i>=0 && s[i]!=' ') i--;
+        while(i>=0 && s[i]!=' ') i--; //cout<<i<<" \n";
         return j-i;
     }
 };
