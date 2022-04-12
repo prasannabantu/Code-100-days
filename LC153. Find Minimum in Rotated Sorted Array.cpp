614d0d8b7b2d20a30797c45adb948aada1c53036
prasannabantu <25prasanna25@gmail.com>
2022-04-12 16:29:50 +0530

    align

--- LC153. Find Minimum in Rotated Sorted Array.cpp
+++ LC153. Find Minimum in Rotated Sorted Array.cpp
@@ -1,10 +1,10 @@
 int findMin(vector<int>& nums) {
-       int i=0; int n=nums.size();
-       if(n==1) return nums[0];
-       int j=n-1;
-       if(nums[i]<nums[j])
-           return nums[i];
-       while(nums[i]<nums[i+1]) i++;
-       return nums[i+1];
+        int i=0; int n=nums.size();
+        if(n==1) return nums[0];
+        int j=n-1;
+        if(nums[i]<nums[j])
+                return nums[i];
+        while(nums[i]<nums[i+1]) i++;
+        return nums[i+1];
 
-   }
+}
