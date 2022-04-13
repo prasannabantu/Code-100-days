105ea892a677b7850858180c95e0816676b55a4f
prasannabantu <25prasanna25@gmail.com>
2022-04-13 18:39:16 +0530

    added code

--- LC242. Valid Anagram.cpp
+++ LC242. Valid Anagram.cpp
@@ -1,1 +1,16 @@
-242. Valid Anagram
+// 242. Valid Anagram
+
+class Solution {
+public:
+bool isAnagram(string s, string t) {
+        char a[26]={};
+        char b[26]={};
+        for(auto i:s)
+                a[i-'a']++;
+        for(auto i:t)
+                b[i-'a']++;
+        for(int i=0; i<26; i++)
+                if(a[i]!=b[i]) return 0;
+        return 1;
+}
+};
