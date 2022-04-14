86359de42d20eb884c4594eac7828a22881ad444
prasannabantu <25prasanna25@gmail.com>
2022-04-15 00:50:17 +0530

    LC557. Reverse Words in a String III.cpp

---
+++ LC557. Reverse Words in a String III.cpp
@@ -0,0 +1,22 @@
+// 557. Reverse Words in a String III
+class Solution {
+public:
+string reverseWords(string s) {
+        int n=s.size();
+        if(n==1) return s;
+        string ans="";
+        int i=0; int j=1;
+        while(j<n)
+        {
+                while(j<=n-1 && s[j]!=' ') { cout<<j<<" "; j++; }
+                for(int p=j-1; p>=i; p--)
+                        ans.push_back(s[p]);
+                ans.push_back(' ');
+                i=j+1; j++;
+        }
+        ans.pop_back();
+        return ans;
+
+
+}
+};
