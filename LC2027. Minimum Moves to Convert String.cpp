3f3ac3c4ac1cb87d5818c1384e42fc75e23cc827
prasannabantu <25prasanna25@gmail.com>
2022-04-16 13:09:12 +0530

    Small change

--- LC2027. Minimum Moves to Convert String.cpp
+++ LC2027. Minimum Moves to Convert String.cpp
@@ -1,1 +1,20 @@
-2027. Minimum Moves to Convert String
+// 2027. Minimum Moves to Convert String
+class Solution {
+public:
+int minimumMoves(string s) {
+        int c=0;
+        int n=s.size();
+        for(int i=0; i<n; i++)
+        {
+                if(s[i]=='X')
+                {
+                        s[i]='O';
+                        if(i<n-1) s[i+1]='O';
+                        if(i<n-2) { s[i+2]='O'; i+=2;}
+                        c++;
+
+                }
+        }
+        return c;
+}
+};
