21bce3821b71f7b165feca271e6013b4694b36c8
prasannabantu <25prasanna25@gmail.com>
2022-04-17 11:16:55 +0530

    LC283. Move Zeroes.cpp

---
+++ LC283. Move Zeroes.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+
+void moveZeroes(vector<int>& nums) {
+        int n=nums.size();
+        int nxt=0;
+        for(int i=0; i<n; i++)
+        {
+                if(nums[i]!=0)
+                        nums[nxt++]=nums[i];
+        }
+        for(int i=nxt; i<n; i++)
+                nums[i]=0;
+}
+};
