eba61f86c9f28f6d1715e61983b5be294b3ba30b
prasannabantu <25prasanna25@gmail.com>
2022-04-17 12:22:21 +0530

    LC496. Next Greater Element I.cpp

---
+++ LC496. Next Greater Element I.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+
+        unordered_map<int,int>mp;
+        stack<int>s;
+        for(auto n:nums2)
+        {
+                while(s.size() && s.top() < n)
+                {
+                        mp[s.top()]=n;
+                        s.pop();
+                }
+                s.push(n);
+        }
+        vector<int>ans;
+        for(auto i:nums1)
+        {
+                ans.push_back(mp.count(i) ? mp[i]:-1);
+        }
+        return ans;
+
+}
+};
