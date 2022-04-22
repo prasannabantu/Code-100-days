b8e14525fb4fcfa9b955f1fe0cda1055923e7caa
prasannabantu <25prasanna25@gmail.com>
2022-04-22 23:56:45 +0530

    LC404. Sum of Left Leaves.cpp

---
+++ LC404. Sum of Left Leaves.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+int sum=0;
+int sumOfLeftLeaves(TreeNode* root,int pre=0) {
+        if(root==NULL) return 0;
+        if(pre==1 && root->left==NULL && root->right==NULL) sum+=root->val;
+        sumOfLeftLeaves(root->left,1);
+        sumOfLeftLeaves(root->right,0);
+        return sum;
+}
+};
