c04f8bbc7034157a3d43a546e3f859ed7c0e99af
prasannabantu <25prasanna25@gmail.com>
2022-04-24 19:57:45 +0530

    small change

--- LC1413. Minimum Value to Get Positive Step by Step Sum.cpp
+++ LC1413. Minimum Value to Get Positive Step by Step Sum.cpp
@@ -1,13 +1,14 @@
 int minStartValue(vector<int>& nums) {
         int mini=nums[0]; int sum=0;
         int n=nums.size();
-        for(int i=0;i<n;i++)
+        for(int i=0; i<n; i++)
         {
 
-            mini=min(mini,sum);
-            sum+=nums[i];
+                mini=min(mini,sum);
+                sum+=nums[i];
         }
         mini=min(mini,sum);
         if(mini>0) return mini;
         if(mini==0) return 1;
         return -mini+1;
+}
