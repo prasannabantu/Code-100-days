7931b222cb0c9cccdda907095a2bdc7398fa4d9d
prasannabantu <25prasanna25@gmail.com>
2022-04-25 23:12:59 +0530

    change 2 space removal

--- LC2125 Number of Laser Beams in a Bank.cpp
+++ LC2125 Number of Laser Beams in a Bank.cpp
@@ -2,22 +2,20 @@
 
 class Solution {
 public:
-    int numberOfBeams(vector<string>& bank) {
+int numberOfBeams(vector<string>& bank) {
         int n=bank.size();
         int count=0;
         int sum=0;
         int temp=0;
         for(auto i:bank)
         {
-            for(auto k:i)
-                if(k=='1') count++;
-            sum+=temp*count;
-            if(count!=0) temp=count;
-            count=0;
+                for(auto k:i)
+                        if(k=='1') count++;
+                sum+=temp*count;
+                if(count!=0) temp=count;
+                count=0;
         }
 
-
-
         return sum;
-    }
+}
 };
