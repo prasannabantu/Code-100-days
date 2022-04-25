0668c234ca0b785f667a75017aa13b31655721ec
prasannabantu <25prasanna25@gmail.com>
2022-04-25 13:42:10 +0530

    small change

--- LC1913. Maximum Product Difference Between Two Pairs.cpp
+++ LC1913. Maximum Product Difference Between Two Pairs.cpp
@@ -1,1 +1,9 @@
-1913. Maximum Product Difference Between Two Pairs
+// 1913. Maximum Product Difference Between Two Pairs
+class Solution {
+public:
+int maxProductDifference(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        return nums[n-1]*nums[n-2]-nums[0]*nums[1];
+}
+};
