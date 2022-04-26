45498bbe0b55d8434b8e36b24ae4ceedb184bdc3
prasannabantu <25prasanna25@gmail.com>
2022-04-26 13:25:17 +0530

    faster solution

--- LC83. Remove Duplicates from Sorted List.cpp
+++ LC83. Remove Duplicates from Sorted List.cpp
@@ -26,3 +26,18 @@ ListNode* deleteDuplicates(ListNode* head) {
         return head;
 }
 };
+
+ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* t=head;
+        ListNode* r=head;
+        while(r!=NULL)
+        {
+                while(r!=NULL && r->next!=NULL && r->val==r->next->val)
+                {
+                        r->next=r->next->next;
+                }
+                if(r!=NULL)
+                        r=r->next;
+        }
+        return head;
+}
