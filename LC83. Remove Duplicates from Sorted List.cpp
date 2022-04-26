4b7e0eaf7b1d028b84b6907c9d0980f0078b7ca2
prasannabantu <25prasanna25@gmail.com>
2022-04-26 13:23:44 +0530

    LC83. Remove Duplicates from Sorted List.cpp

---
+++ LC83. Remove Duplicates from Sorted List.cpp
@@ -0,0 +1,28 @@
+83. Remove Duplicates from Sorted List
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* t=head;
+        ListNode* r=head;
+        while(t!=NULL)
+        {
+                r=t;
+                while(r!=NULL && r->next!=NULL && r->val==r->next->val)
+                {
+                        r->next=r->next->next;
+                }
+                t=t->next;
+        }
+        return head;
+}
+};
