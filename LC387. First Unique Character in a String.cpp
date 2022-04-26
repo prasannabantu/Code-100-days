c320903a33ad8c83a1a89b8d2127d556a1beeabb
prasannabantu <25prasanna25@gmail.com>
2022-04-26 10:08:31 +0530

    LC387. First Unique Character in a String.cpp

---
+++ LC387. First Unique Character in a String.cpp
@@ -0,0 +1,9 @@
+int firstUniqChar(string s) {
+        int cnt[26]={};
+        for(auto c:s)
+                cnt[c-'a']++;
+        int n=s.size();
+        for(int i=0; i<n; i++)
+                if(cnt[s[i]-'a']==1) return i;
+        return -1;
+}
