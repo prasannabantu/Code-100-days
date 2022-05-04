98b12d9dc3753af2f757c5f15caee9735bbfcf0f
prasannabantu <25prasanna25@gmail.com>
2022-05-04 18:21:24 +0530

    Full change

--- LC36. Valid Sudoku.cpp
+++ LC36. Valid Sudoku.cpp
@@ -1,40 +1,24 @@
 class Solution {
 public:
-
-bool isnum(char c)
-{
-        return c<='9' && c>='1';
-}
-
-bool boxcheck(vector<vector<char> >& board,int r,int c)
-{
-        set<int>se;
-        int dirRow[9] = { -1, -1, -1, 0, 0, 0, 1, 1, 1 };
-        int dirCol[9] = { -1, 0, 1, -1, 0, 1, -1, 0, 1 };
-
-
-        for (int i = 0; i < 9; i++)
+bool isValidSudoku(vector<vector<char> >& board) {
+        vector<int>col(9);
+        vector<int>grid(9);
+        for(int i=0; i<9; i++)
         {
-                int nextRow = r + dirRow[i];
-                int nextCol = c + dirCol[i];
-
-        }
-        for(int i=0; i<3; i++)
-                for(int j=0; j<3; j++)
+                int row=0;
+                for(int j=0; j<9; j++)
                 {
-
-                        if(isnum(board[i][j]))
+                        if(board[i][j]!='.')
                         {
-                                int k=se.size();
-                                se.insert(board[i][j]-'0')
-                                if(k==se.size()) return 0;
+                                int t=board[i][j]-'1';
+                                int f=1<<t;
+                                if(row&f || col[j] & f || grid[(i/3)*3+j/3] &f) return 0;
+                                row|=f;
+                                col[j]|=f;
+                                grid[(i/3)*3+j/3]|=f;
                         }
                 }
+        }
         return 1;
-}
-
-bool isValidSudoku(vector<vector<char> >& board)
-{
-
 }
 };
