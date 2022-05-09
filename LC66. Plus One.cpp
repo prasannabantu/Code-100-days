e6b4f0be73f9edfdf07c6178ca3720c9c2d1e882
prasannabantu <25prasanna25@gmail.com>
2022-05-09 16:41:11 +0530

    concise

--- LC66. Plus One.cpp
+++ LC66. Plus One.cpp
@@ -2,31 +2,20 @@ class Solution {
 public:
 vector<int> plusOne(vector<int>& digits) {
         int n=digits.size();
-        int e=digits[n-1]+1;
-        digits[n-1]+=1;
-        for(int i=n-1; i>=1; i--)
+        vector<int>ans;
+        int i=n-1;
+        int carry=1;
+
+        while(i>=0)
         {
-                digits[i]=e%10;
-                if(e>=10)
-                {
-                        e=e/10;
-                        e+=digits[i-1];
-                }
-                else
-                        e=digits[i-1];
+                digits[i]+=carry;
+                ans.push_back((digits[i])%10);
+                carry=digits[i]/10;
+                i--;
         }
-        digits[0]=e;
-        cout<<digits[0]<<"\n";
-        vector<int>ext(n+1);
-        if(digits[0]>=10)
-        {
-                ext[0]=1;
-                ext[1]=0;
-                for(int j=1; j<=n-1; j++)
-                        ext[j+1]=digits[j];
-                return ext;
-        }
-        return digits;
+        if(carry) ans.push_back(carry);
+        reverse(ans.begin(),ans.end());
+        return ans;
 
 }
 };
