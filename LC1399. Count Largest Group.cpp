15781df63ff4d9f280a6c099719447bd7fae6070
prasannabantu <25prasanna25@gmail.com>
2022-05-13 14:58:43 +0530

    // 598. Range Addition II

--- LC1399. Count Largest Group.cpp
+++ LC1399. Count Largest Group.cpp
@@ -1,1 +1,26 @@
-1399. Count Largest Group
+// 1399. Count Largest Group
+class Solution {
+public:
+
+int cdig(int n)
+{
+        int sum=0;
+        while(n)
+        {
+                sum+=n%10; n/=10;
+        }
+        return sum;
+}
+
+int countLargestGroup(int n)
+{
+        vector<int>v(37,0);
+        for(int i=1; i<=n; i++)
+                v[cdig(i)]++;
+        int k=*max_element(v.begin(),v.end());
+        int c=0;
+        for(auto i:v)
+                if(k==i) c++;
+        return c;
+}
+};
