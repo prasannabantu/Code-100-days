c067cb560ad27947d833ff6ecd510098c382976b
prasannabantu <25prasanna25@gmail.com>
2022-05-14 01:18:36 +0530

    small change

--- LC598. Range Addition II.cpp
+++ LC598. Range Addition II.cpp
@@ -1,1 +1,15 @@
-598. Range Addition II
+// 598. Range Addition II
+class Solution {
+public:
+int maxCount(int m, int n, vector<vector<int> >& ops) {
+        int mini1=m;
+        int mini2=n;
+        for(auto a:ops)
+        {
+                mini1=min(mini1,a[0]);
+                mini2=min(mini2,a[1]);
+        }
+        return mini1*mini2;
+
+}
+};
