ac1f15011be46de0946e2687ffaea339edee43ac
prasannabantu <25prasanna25@gmail.com>
2022-05-17 17:34:09 +0530

    new

--- LC2068. Check Whether Two Strings are Almost Equivalent.cpp
+++ LC2068. Check Whether Two Strings are Almost Equivalent.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
 bool checkAlmostEquivalent(string word1, string word2) {
+        int cnt[26]={};
+        int n=word1.size();
+        for(int i=0; i<n; i++)
+        {
+                cnt[word1[i]-'a']++;
+                cnt[word2[i]-'a']--;
+        }
+
+        for(int i=0; i<26; i++)
+                if(abs(cnt[i])>3) return 0;
+        return 1;
+
+}
+};
+
+class Solution {
+public:
+bool checkAlmostEquivalent(string word1, string word2) {
         int acnt[26]={};
         int bcnt[26]={};
         for(auto c:word1) acnt[c-'a']++;
@@ -13,6 +31,6 @@ bool checkAlmostEquivalent(string word1, string word2) {
         return 1;
 
 
-        return 1;
+
 }
 };
