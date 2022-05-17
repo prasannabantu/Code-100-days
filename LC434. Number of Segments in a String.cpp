fbabe6c41c8685c0ba02aa91efa91e8f5e937883
prasannabantu <25prasanna25@gmail.com>
2022-05-17 11:10:54 +0530

    Small and simple

--- LC434. Number of Segments in a String.cpp
+++ LC434. Number of Segments in a String.cpp
@@ -2,6 +2,25 @@ class Solution {
 public:
 int countSegments(string s) {
         int i=0;
+        s.push_back(' ');
+        int n=s.size();
+        int c=0;
+        for(int i=0; i<n; i++)
+        {
+                if(s[i]==' ')
+                {
+                        if(i>=1 && s[i-1]!=' ') c++;
+                }
+        }
+        return c;
+}
+};
+
+
+class Solution {
+public:
+int countSegments(string s) {
+        int i=0;
         int n=s.size();
         if(n==0) return 0;
         int j=n-1;
