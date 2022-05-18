8d5d6dc03a1d6c2690005f7b13e7b507a1c409be
prasannabantu <25prasanna25@gmail.com>
2022-05-18 11:07:14 +0530

    Without using exta array

--- LC746. Min Cost Climbing Stairs.cpp
+++ LC746. Min Cost Climbing Stairs.cpp
@@ -12,3 +12,32 @@ int minCostClimbingStairs(vector<int>& cost) {
 
 }
 };
+
+
+class Solution {
+public:
+int minCostClimbingStairs(vector<int>& cost) {
+        int n=cost.size();
+        vector<int>dp=cost;
+        for(int i=2; i<n; i++)
+        {
+                dp[i]=min(dp[i-1],dp[i-2])+dp[i];
+        }
+        return min(dp[n-1],dp[n-2]);
+}
+};
+
+class Solution {
+public:
+int rob(vector<int>& nums) {
+        int n=nums.size();
+        int h1=0; int h2=0; int c=0;
+        for(auto n:nums)
+        {
+                c=max(h2,h1+n);
+                h1=h2;
+                h2=c;
+        }
+        return c;
+}
+};
