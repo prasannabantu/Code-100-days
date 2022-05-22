fdb05d4c5cb81351486ec102846ed99016bff1bf
prasannabantu <25prasanna25@gmail.com>
2022-05-22 23:29:39 +0530

    LC89. Gray Code.cpp

---
+++ LC89. Gray Code.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+vector<int> grayCode(int n) {
+        vector<int>ans;
+        for(int i=0; i<=(int)pow(2,n)-1; i++)
+        {
+                ans.push_back(i^(i>>1));
+        }
+        return ans;
+}
+};
