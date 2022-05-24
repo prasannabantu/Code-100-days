52bf81dced87d7b15ebfb32646970ad3d9da5b53
prasannabantu <25prasanna25@gmail.com>
2022-05-24 14:56:30 +0530

    LC1646. Get Maximum in Generated Array.cpp

---
+++ LC1646. Get Maximum in Generated Array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+int getMaximumGenerated(int n) {
+        if(n<=1) return n;
+        vector<int>nums(n+1,0);
+        nums[0]=0;
+        nums[1]=1;
+
+        int maxii=0;
+        for(int i=2; i<=n; i++)
+        {
+                if(i%2==0) nums[i]=nums[i/2];
+                else nums[i]=nums[i/2]+nums[(i+1)/2];
+                maxii=max(nums[i],maxii);
+        }
+        return maxii;
+
+}
+};
