0bf5becfcb3ced0700d01d7d32bb9241e7d26dc9
prasannabantu <25prasanna25@gmail.com>
2022-06-18 11:47:52 +0530

    Leetcode 75 Level 1- Problem 1

--- LC1480. Running Sum of 1d Array.cpp
+++ LC1480. Running Sum of 1d Array.cpp
@@ -6,3 +6,16 @@ vector<int> runningSum(vector<int>& nums) {
         return nums;
 }
 };
+
+
+class Solution {
+public:
+vector<int> runningSum(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=1; i<n; i++)
+        {
+                nums[i]+=nums[i-1];
+        }
+        return nums;
+}
+};
