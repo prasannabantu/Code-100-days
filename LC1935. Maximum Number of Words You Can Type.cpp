880996e3bfaec875ecf555c76afb12c5ffff014e
prasannabantu <25prasanna25@gmail.com>
2022-06-21 06:36:36 +0530

    little comment

--- LC1935. Maximum Number of Words You Can Type.cpp
+++ LC1935. Maximum Number of Words You Can Type.cpp
@@ -18,3 +18,29 @@ int canBeTypedWords(string text, string brokenLetters) {
 
 }
 };
+
+
+class Solution {
+public:
+int canBeTypedWords(string text, string brokenLetters) {
+        int cnt=0;
+        stringstream s(text);
+        string word;
+        bitset<26>cnt;
+        for(auto c:brokenLetters)
+        {
+                cnt[c-'a']=1;
+        }
+        while(s>>word)
+        {
+                int f=1;
+                for(auto c:word)
+                {
+                        if(cnt[c-'a']) f=0;
+                }
+                cnt+=f;
+        }
+        return cnt;
+
+}
+};
