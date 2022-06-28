852b9f9058edb41e827d1e9e72a232929dd319f0
prasannabantu <25prasanna25@gmail.com>
2022-06-28 11:28:56 +0530

    faster solution

--- LC1433. Check If a String Can Break Another String.cpp
+++ LC1433. Check If a String Can Break Another String.cpp
@@ -18,3 +18,39 @@ bool checkIfCanBreak(string s1, string s2) {
         return f1|f2;
 }
 };
+
+class Solution {
+public:
+bool checkIfCanBreak(string s1, string s2) {
+        int acnt[26]={};
+        int bcnt[26]={};
+        int n=s1.size();
+        bool f1=1,f2=1;
+        for(int i=0; i<n; i++)
+        {
+                acnt[s1[i]-'a']++;
+                bcnt[s2[i]-'a']++;
+        }
+
+        int sum1=0;
+        int sum2=0;
+        for(int i=0; i<26; i++)
+        {
+                sum1+=acnt[i];
+                sum2+=bcnt[i];
+                if(sum1>sum2) f1=0;
+        }
+
+        sum1=0;
+        sum2=0;
+        for(int i=0; i<26; i++)
+        {
+                sum1+=acnt[i];
+                sum2+=bcnt[i];
+                if(sum1<sum2) f2=0;
+        }
+
+        return f1|f2;
+
+}
+};
