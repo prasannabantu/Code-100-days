cc0c101d9c0891cdbac85e77dd3352088ff2d062
prasannabantu <25prasanna25@gmail.com>
2022-07-02 18:35:00 +0530

    Just comment

--- LC1464. Maximum Product of Two Elements in an Array.cpp
+++ LC1464. Maximum Product of Two Elements in an Array.cpp
@@ -5,3 +5,27 @@ int maxProduct(vector<int>& nums) {
         return (nums[nums.size()-1]-1)*(nums[nums.size()-2]-1);
 }
 };
+
+class Solution {
+public:
+int maxProduct(vector<int>& nums) {
+        int n= nums.size();
+        int maxi1=0;
+        int maxi2=INT_MIN;
+        for(int i=0; i<nums.size(); i++)
+        {
+                if(nums[i]>=maxi1)
+                {
+                        maxi2=maxi1;
+                        maxi1=nums[i];
+
+                }
+                else
+                if(nums[i]>maxi2)
+                {
+                        maxi2=nums[i];
+                }
+        }
+        return (maxi1-1)*(maxi2-1);
+}
+};
