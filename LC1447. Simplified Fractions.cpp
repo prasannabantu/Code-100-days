8eca3d593fc0bc371f8f56ef101497144be427e5
prasannabantu <25prasanna25@gmail.com>
2022-07-02 17:53:14 +0530

    little bit faster

--- LC1447. Simplified Fractions.cpp
+++ LC1447. Simplified Fractions.cpp
@@ -16,3 +16,22 @@ vector<string> simplifiedFractions(int n) {
         return ans;
 }
 };
+
+
+class Solution {
+public:
+vector<string> simplifiedFractions(int n) {
+        vector<string>ans;
+        for(int i=1; i<=n-1; i++)
+        {
+                for(int j=i+1; j<=n; j++)
+                {
+                        if(__gcd(i,j)==1)
+                                ans.push_back(to_string(i)+'/'+to_string(j));
+                }
+        }
+
+
+        return ans;
+}
+};
