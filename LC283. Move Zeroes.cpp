084f1a6685429e160048146c9e0516c3665edecd
prasannabantu <25prasanna25@gmail.com>
2022-07-08 17:13:25 +0530

    small edited solution

--- LC283. Move Zeroes.cpp
+++ LC283. Move Zeroes.cpp
@@ -13,3 +13,16 @@ void moveZeroes(vector<int>& nums) {
                 nums[i]=0;
 }
 };
+
+class Solution {
+public:
+void moveZeroes(vector<int>& nums) {
+        for(int last=0,cur=0; cur<nums.size(); cur++)
+        {
+                if(nums[cur]!=0)
+                        swap(nums[cur],nums[last++]);
+        }
+
+
+}
+};
