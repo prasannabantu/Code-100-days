1a6a021b753419ff0449b64d5855d05775b8529f
prasannabantu <25prasanna25@gmail.com>
2022-07-09 10:13:26 +0530

    comment with reverse

--- LC557. Reverse Words in a String III.cpp
+++ LC557. Reverse Words in a String III.cpp
@@ -20,3 +20,22 @@ string reverseWords(string s) {
 
 }
 };
+
+class Solution {
+public:
+string reverseWords(string s) {
+        stringstream ss(s);
+        string word;
+        string res;
+        while(ss>>word)
+        {
+                reverse(word.begin(),word.end());
+                res+=word;
+                res+=" ";
+        }
+        res.pop_back();
+        return res;
+
+
+}
+};
