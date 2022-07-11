2a48cdd9bb72a38a35828d8b21c0782e31d03149
prasannabantu <25prasanna25@gmail.com>
2022-07-11 08:31:43 +0530

    level order

--- LC199. Binary Tree Right Side View.cpp
+++ LC199. Binary Tree Right Side View.cpp
@@ -25,3 +25,44 @@ vector<int> rightSideView(TreeNode* root) {
         return res;
 }
 };
+
+
+
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+vector<int> rightSideView(TreeNode* root) {
+        vector<int>res;
+        queue<TreeNode*>q;
+        q.push(root);
+        if(!root) return {};
+        while(!q.empty())
+        {
+                int n=q.size();
+                TreeNode* k;
+                for(int i=0; i<n; i++)
+                {
+                        k=q.front(); q.pop();
+                        if(k->left) q.push(k->left);
+                        if(k->right) q.push(k->right);
+                }
+                res.push_back(k->val);
+        }
+        return res;
+}
+};
