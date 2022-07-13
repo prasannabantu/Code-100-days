c0b3d942e672a026ac5cb80f374e0af3f9ae8f83
prasannabantu <25prasanna25@gmail.com>
2022-07-13 06:50:44 +0530

    Small cahnge

--- LC567. Permutation in String.cpp
+++ LC567. Permutation in String.cpp
@@ -26,3 +26,42 @@ bool checkInclusion(string s1, string s2) {
         return 0;
 }
 };
+
+
+
+
+
+
+
+
+class Solution {
+public:
+bool cmp(int a[],int b[])
+{
+        for(int i=0; i<26; i++)
+                if(a[i]!=b[i]) return 0;
+        return 1;
+}
+
+bool checkInclusion(string s1, string s2) {
+        int acnt[26]={};
+        int bcnt[26]={};
+        int m=s1.size();
+        int n=s2.size();
+        if(m>n) return 0;
+
+        for(int i=0; i<m; i++)
+        {
+                acnt[s1[i]-'a']++;
+                bcnt[s2[i]-'a']++;
+        }
+        if(cmp(acnt,bcnt)) return 1;
+        for(int i=m; i<n; i++)
+        {
+                bcnt[s2[i-m]-'a']--;
+                bcnt[s2[i]-'a']++;
+                if(cmp(acnt,bcnt)) return 1;
+        }
+        return 0;
+}
+};
