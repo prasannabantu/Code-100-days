a25833f5d59c99c7550d8e8fb5206d017cfabb0e
prasannabantu <25prasanna25@gmail.com>
2022-07-13 18:40:13 +0530

    Using queue

--- LC102. Binary Tree Level Order Traversal.cpp
+++ LC102. Binary Tree Level Order Traversal.cpp
@@ -26,3 +26,40 @@ vector<vector<int> > levelOrder(TreeNode* root) {
         return ans;
 }
 };
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+vector<vector<int> > levelOrder(TreeNode* root) {
+        vector<vector<int> >ans;
+        if(root==NULL) return ans;
+        queue<TreeNode*>Q;
+        Q.push(root);
+        while(!Q.empty())
+        {
+                vector<int>res;
+                for(int i=Q.size(); i>0; i--)
+                {
+                        TreeNode* k=Q.front(); Q.pop();
+                        res.push_back(k->val);
+                        if(k->left) Q.push(k->left);
+                        if(k->right) Q.push(k->right);
+                }
+                ans.push_back(res);
+        }
+        return ans;
+}
+};
