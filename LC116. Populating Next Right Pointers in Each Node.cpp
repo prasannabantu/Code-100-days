63aec9891806b21aeb6131510beda3922debfee0
prasannabantu <25prasanna25@gmail.com>
2022-07-15 10:22:04 +0530

    LC116. Populating Next Right Pointers in Each Node

---
+++ LC116. Populating Next Right Pointers in Each Node.cpp
@@ -0,0 +1,44 @@
+/*
+   // Definition for a Node.
+   class Node {
+   public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+   };
+ */
+
+class Solution {
+public:
+Node* connect(Node* t) {
+        queue<Node*>q;
+        if(t==NULL) return t;
+        t->next=NULL;
+        Node* root=t; t->next=NULL;
+        q.push(t);
+        Node* tmp;
+        while(!q.empty())
+        {
+                int n=q.size();
+                for(int i=0; i<n; i++)
+                {
+                        Node* t=q.front();
+                        q.pop();
+                        if(t->left) q.push(t->left);
+                        if(t->right) q.push(t->right);
+                        if(i==n-1) t->next=NULL;
+                        else
+                                t->next=q.front();
+                }
+        }
+        return root;
+}
+};
