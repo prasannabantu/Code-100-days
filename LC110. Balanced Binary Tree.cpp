ab64dea39aee317d38af8482111b38204535ebf1
prasannabantu <25prasanna25@gmail.com>
2022-07-22 07:29:34 +0530

    second attempt

--- LC110. Balanced Binary Tree.cpp
+++ LC110. Balanced Binary Tree.cpp
@@ -1,14 +1,27 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
+//Second attempt
+class Solution {
+public:
+int findHeight(TreeNode* root)
+{
+        if(root)
+        {
+                return 1+max(findHeight(root->left),findHeight(root->right));
+        }
+        return 0;
+}
+
+bool isBalanced(TreeNode* root) {
+        if(root==NULL) return 1;
+        return abs(findHeight(root->left)-findHeight(root->right))<=1 && isBalanced(root->left) && isBalanced(root->right);
+}
+};
+
+
+
+
+
+// First attempt
 class Solution {
 public:
 int findH(TreeNode* root)
