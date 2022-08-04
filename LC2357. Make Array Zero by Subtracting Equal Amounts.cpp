3a0a441579acfc5d984b422e70bc15a76a32ac90
prasannabantu <25prasanna25@gmail.com>
2022-08-04 22:19:12 +0530

    LC2357. Make Array Zero by Subtracting Equal Amounts.cpp

---
+++ LC2357. Make Array Zero by Subtracting Equal Amounts.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+int minimumOperations(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        for(auto num:nums) if(num!=0) mp[num]++;
+        return mp.size();
+}
+};
